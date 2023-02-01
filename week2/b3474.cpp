43da07c5d188aa5dbcb9f4b599f7092c5e6b2bbd
parkjungsun <pjss7373@gmail.com>
2023-02-01 22:47:11 +0900

    sss

---
+++ week2/b3474.cpp
@@ -0,0 +1,29 @@
+/* 백준 3474 | 비밀번호 발음하기 https://www.acmicpc.net/problem/3474 */
+
+/* 
+*/
+
+#include<bits/stdc++.h>
+#include<string.h>
+#include<algorithm>
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int T;
+    cin >> T;
+
+
+    for(int i=0; i<T; i++) {
+
+    } 
+
+    return 0;
+}
+
+
