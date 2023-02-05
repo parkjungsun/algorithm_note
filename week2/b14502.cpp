4da94bda5770bd2458e00a9d9e57ad0fb31d61e5
parkjungsun <pjss7373@gmail.com>
2023-02-05 22:27:11 +0900

    week2 b14502

---
+++ week2/b14502.cpp
@@ -0,0 +1,21 @@
+/* 백준 14502 | 연구소 https://www.acmicpc.net/problem/14502 */
+
+/* 
+*/
+
+#include<bits/stdc++.h>
+#include<string.h>
+#include<algorithm>
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    return 0;
+}
+
+
