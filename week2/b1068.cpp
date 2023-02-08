3f0f4963edcc7d56a7e0fe0871e1fbe04b745298
parkjungsun <pjss7373@gmail.com>
2023-02-08 21:54:57 +0900

    week2 b1068

---
+++ week2/b1068.cpp
@@ -0,0 +1,21 @@
+/* 백준 1068 | 치즈 https://www.acmicpc.net/problem/1068 */
+
+/* 
+*/
+
+#include<bits/stdc++.h>
+#include<string.h>
+#include<algorithm>
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    return 0;
+}
+
+
